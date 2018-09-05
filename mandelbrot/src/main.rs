7a9f8946ac8eae2d95b98f49d251d4d6ced57143
KeisukeToyota <hm.pudding0715@gmai.com>
2018-09-05 21:30:00 +0900

    2.6.2まで

---
+++ mandelbrot/src/main.rs
@@ -0,0 +1,60 @@
+extern crate num;
+use num::Complex;
+use std::str::FromStr;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn square_loop(mut x: f64) {
+    loop {
+        x = x * x;
+    }
+}
+
+fn square_add_loop(c: f64) {
+    let mut x = 0.;
+    loop {
+        x = x * x + c;
+    }
+}
+
+#[allow(dead_code)]
+fn complex_square_add_loop(c: Complex<f64>) {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    loop {
+        z = z * z + c;
+    }
+}
+
+fn escape_time(c: Complex<f64>, limit: u32)-> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char)-> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]). T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
+fn parse_complex(s: &str)-> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None
+    }
+}
+
